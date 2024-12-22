06242c34aefaa19e6b63da6140ccb80cd48a7f68
Yasmine Eleschi <yasmineleschi01@gmail.com>
2024-12-22 12:25:20 +0100

    deploy

--- Config/DbConnection.js
+++ Config/DbConnection.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+    const connect = await mongoose.connect(process.env.MONGO_URI);
     console.log(
       "Database connected:",
       connect.connection.host,
       connect.connection.name
     );
+    console.log('MongoDB connecté avec succès');
   } catch (err) {
     console.error("Error connecting to the database:", err.message);
     process.exit(1);
